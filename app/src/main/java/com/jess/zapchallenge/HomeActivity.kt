cf559403d05c3df5205ac6da67c0686cd4225dc0
jeehmilena <jessicamilenacorrea@gmail.com>
2020-10-09 12:38:30 -0300

    criação HomeFragment.kt

--- app/src/main/java/com/jess/zapchallenge/HomeActivity.kt
+++ app/src/main/java/com/jess/zapchallenge/HomeActivity.kt
@@ -3,11 +3,11 @@ package com.jess.zapchallenge
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_home)
 
     }
 }
